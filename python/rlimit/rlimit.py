f6daa3b010e12024e4d470d323bc7bf57ca56e49
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2012-05-03 19:40:47 +0200

    Setting up the python packages a bit more nicely.

--- python/rlimit/rlimit.py
+++ python/rlimit/rlimit.py
@@ -1,6 +1,6 @@
 '''
-Subprocess is Python module for running, controlling and profiling
-subprocesses in an automatic manner. It is primarely designed for
+rlimit is Python module for running, controlling and profiling
+subprocesses in an automatic manner. It is primarily designed for
 black-box testing purpose in an educational context.
 '''
 
@@ -184,34 +184,3 @@ class Subprocess(object):
         Terminate the subprocess and returns its return code.
         '''
         return self.process.terminate()
-
-
-
-# Examples... to be removed later
-
-print('Running...')
-print("")
-__process__ = Subprocess(['sleep', '3'])
-
-print("First run:")
-__process__.run(memory=50000)
-__process__.wait()
-print("Return code: %d" % __process__.returncode)
-print("Execution time: %f" % __process__.real_time)
-print("User time: %f" % __process__.user_time)
-print("System time: %f" % __process__.sys_time)
-print("Memory: %f" % __process__.memory)
-print("")
-
-print("Second run:")
-__process__.run(timeout=5)
-__process__.wait()
-print("Return code: %d" % __process__.returncode)
-print("Execution time: %f" % __process__.real_time)
-print("")
-
-print("Third run:")
-__process__.run(timeout=1)
-__process__.wait()
-print("Return code: %d" % __process__.returncode)
-print("Execution time: %f" % __process__.real_time)
