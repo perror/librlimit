5faa48b6018e556d6964185e5c4b3fd9ca77de50
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2012-06-21 15:31:21 +0200

    Fixing an unecessary message

--- autogen.sh
+++ autogen.sh
@@ -87,13 +87,6 @@ if test "$DIE" -eq 1; then
   exit 1
 fi
 
-if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
-  echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
-  echo
-fi
-
 case $CC in
 xlc )
   am_opt=--include-deps;;
@@ -134,9 +127,15 @@ done
 conf_flags="--enable-maintainer-mode"
 
 if test x$DOCONFIGURE = xyes; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`\(g\)make\' to compile. || exit 1
+    if test -z "$*"; then
+	echo "**Warning**: I am going to run \`configure' with no arguments."
+	echo "If you wish to pass any to it, please specify them on the"
+	echo \`$0\'" command line."
+	echo
+    fi
+    echo Running $srcdir/configure $conf_flags "$@" ...
+    $srcdir/configure $conf_flags "$@" \
+	&& echo Now type \`\(g\)make\' to compile. || exit 1
 else
-  echo Skipping configure process.
+    echo Skipping configure process.
 fi
